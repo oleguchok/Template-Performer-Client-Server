b2b1e5ec118ef20e97cc149ee61c597c05d8853a
oleguhcok <oleguchok@gmail.com>
2015-07-17 13:01:09 +0300

    Add template class and strategy pattern

---
+++ TemplateLibrary/TemplateLibrary/Template.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateLibrary.Strategy;
+
+namespace TemplateLibrary
+{   
+    public class Template
+    {
+        private IStrategy _strategy;
+
+        public Template(IStrategy strategy, String templateText, String[] namespaces,
+                        params Variable[] variables)
+        {
+            _strategy = strategy;
+        }
+
+        public IStrategy Strategy
+        {
+            set { _strategy = value; }
+        }
+    }
+}
---
+++ TemplateLibrary/TemplateLibrary/Variable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateLibrary
+{
+    public class Variable
+    {
+        public Variable(String name, ArgumentType type)
+        {
+            
+        }
+    }
+}
