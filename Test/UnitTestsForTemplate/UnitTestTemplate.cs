004fbab11aa909f8e160f28887b75fd5e67dfe82
oleguhcok <oleguchok@gmail.com>
2015-07-18 21:50:07 +0300

    Add test for simple string in csharp template

---
+++ Test/UnitTestsForTemplate/UnitTestTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateLibrary;
+using TemplateLibrary.Strategy;
+using System.IO;
+
+namespace UnitTestsForTemplate
+{
+    [TestClass]
+    public class UnitTestTemplate
+    {
+        [TestMethod]
+        public void Can_Identify_Simple_String()
+        {
+            using (var template = new Template(new CSharp(), "aba", new String[0])) 
+            using (var output = new StringWriter())
+            {
+                template.Render(output);
+                Assert.AreEqual("aba", output.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void TestServer()
+        {
+
+        }
+    }
+}
